a225f946a99a2aba2c833fdaa244756bd6bdff09
Anette kvarving <anettekvarving@MacBook-Pro-70.domain_not_set.invalid>
2022-01-28 15:25:23 +0100

    nye filer inn

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/anettekva/rivercrossing
+
+go 1.17
---
+++ state/state.java
@@ -0,0 +1,5 @@
+class state {
+    public static void main(String args[]) {
+        System.out.println("Hallo, alle sammen!");
+    }
+}
